e4e54e49cd3fb7ce8d331720978d46cf6e62b9ba
Javier Gutierrez <devielsteam05@gmail.com>
2025-06-24 20:57:53 -0500

    Avance de la clase enemigo

---
+++ Enemigo.java
@@ -0,0 +1,10 @@
+public class Enemigo{
+    private int x;
+    private int y;
+    private char simbolo ='E';
+    
+    public Enemigo(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
